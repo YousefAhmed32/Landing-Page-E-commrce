3e836ec83971821631b73120389beaaa81e724c5
Yousef Ahmed <my10424231@gmail.com>
2025-07-26 10:15:10 +0300

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -198,7 +198,7 @@ function opencart() {
   }
 }
 
-let nameDuser = document.querySelector("#user_data")
+let nameUser = document.querySelector("#user_data")
 let userinfo = document.querySelector("#user-form")
 let links = document.querySelector("#links")
 
@@ -206,7 +206,7 @@ let links = document.querySelector("#links")
 if (localStorage.getItem("username")) {
   links.remove()
   userinfo.style.display = "flex";
-  nameDuser.innerHTML = localStorage.getItem("username")
+  nameUser.innerHTML = localStorage.getItem("username")
   console.log("hello home")
 
 }
--- js/checkname.js
+++ js/checkname.js
@@ -1,10 +1,12 @@
-let nameDuser = document.querySelector("#user_data")
+let nameUser = document.querySelector("#user_data")
 let userinfo = document.querySelector("#user-form")
 let links = document.querySelector("#links")
 if (localStorage.getItem("username")) {
   links.remove()
   userinfo.style.display = "flex";
-  nameDuser.innerHTML = localStorage.getItem("username")
+  userinfo.style.width = "auto";
+  userinfo.style.margin = "0px 20px";
+  nameUser.innerHTML = localStorage.getItem("username")
   console.log("hello home")
 
 }
